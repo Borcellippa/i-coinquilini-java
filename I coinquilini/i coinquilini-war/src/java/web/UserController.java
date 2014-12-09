915d9a5967dda84bc3056e70503ea1f0f9a00eda
Bortignon Gianluca <g.bortignon@gmail.com>
2014-12-09 12:15:30 +0100

    Set checkbox genere

--- I coinquilini/i coinquilini-war/src/java/web/UserController.java
+++ I coinquilini/i coinquilini-war/src/java/web/UserController.java
@@ -318,8 +318,7 @@ public class UserController extends HttpServlet {
                 if (!"".equals(location)) {
                     user.setCitta_natale(location);
                 }
-                if(user.getFoto_path() == "images/user.png")
-                    user.setFoto_path(imageUrl);
+                user.setFoto_path(imageUrl);
 
                 gestoreUtente.addUtente(user);
                 needPwd = true;
