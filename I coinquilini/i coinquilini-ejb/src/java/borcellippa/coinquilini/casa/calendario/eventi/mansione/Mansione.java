e8994edb41fbb2719629852ccbf4be442be842e2
Bortignon Gianluca <g.bortignon@gmail.com>
2014-12-09 17:26:35 +0100

    Eliminato id Mansione

--- I coinquilini/i coinquilini-ejb/src/java/borcellippa/coinquilini/casa/calendario/eventi/mansione/Mansione.java
+++ I coinquilini/i coinquilini-ejb/src/java/borcellippa/coinquilini/casa/calendario/eventi/mansione/Mansione.java
@@ -9,9 +9,6 @@ package borcellippa.coinquilini.casa.calendario.eventi.mansione;
 import borcellippa.coinquilini.casa.calendario.eventi.evento.Evento;
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  *
@@ -20,9 +17,6 @@ import javax.persistence.Id;
 @Entity
 public class Mansione extends Evento implements Serializable{
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private String durata;
 
     /**
@@ -41,39 +35,5 @@ public class Mansione extends Evento implements Serializable{
      */
     public void setDurata(String durata) {
         this.durata = durata;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Mansione)) {
-            return false;
-        }
-        Mansione other = (Mansione) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "ejb.coinquilini.casa.calendario.eventi.mansione.mansione[ id=" + id + " ]";
-    }
-    
+    }  
 }
