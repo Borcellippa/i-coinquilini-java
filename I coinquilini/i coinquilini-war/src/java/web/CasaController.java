70ded35b8e907e3e0a0cb7208475ffd4c6a844ba
Mattia Sappa <mattiasappa@gmail.com>
2014-12-10 11:50:23 +0100

    add CasaController

--- I coinquilini/i coinquilini-war/src/java/web/CasaController.java
+++ I coinquilini/i coinquilini-war/src/java/web/CasaController.java
@@ -5,18 +5,31 @@
  */
 package web;
 
+import borcellippa.coinquilini.casa.casa.GestoreCasaLocal;
+import borcellippa.coinquilini.users.inquilino.GestoreInquilinoLocal;
+import borcellippa.coinquilini.users.utente.GestoreUtenteLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static utility.Utility.buildGson;
 
 /**
  *
  * @author Mattia
  */
 public class CasaController extends HttpServlet {
+
+    @EJB
+    private GestoreInquilinoLocal gestoreInquilino;
+    @EJB
+    private GestoreUtenteLocal gestoreUtente;
+    @EJB
+    private GestoreCasaLocal gestoreCasa;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,6 +45,32 @@ public class CasaController extends HttpServlet {
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+
+        RequestDispatcher rd;
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            request.setAttribute("location", buildGson("home"));
+            rd = getServletContext().getRequestDispatcher("/WEB-INF/pages/home/home.jsp");
+
+        } else if (action.equals("creaCasa")) {
+
+            request.setAttribute("location", buildGson("creaCasa"));
+            rd = getServletContext().getRequestDispatcher("/WEB-INF/pages/house/creaCasa.jsp");
+
+        } else if (action.equals("entraInCasa")) {
+            
+            
+            request.setAttribute("location", buildGson("casa"));
+            rd = getServletContext().getRequestDispatcher("/WEB-INF/pages/house/casa.jsp");
+        } else { // caso in cui non ci sia nessuna action da eseguire
+            request.setAttribute("location", buildGson("error_page"));
+            request.setAttribute("errorPage", buildGson("no_action"));
+            rd = getServletContext().getRequestDispatcher("/WEB-INF/pages/templates/errore.jsp");
+        }
+
+        rd.forward(request, response);
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ I coinquilini/i coinquilini-war/web/WEB-INF/pages/house/creaCasa.jsp
@@ -0,0 +1,5 @@
+<%-- 
+    Document   : creaCasa
+    Created on : 10-dic-2014, 11.44.50
+    Author     : Mattia
+--%>
